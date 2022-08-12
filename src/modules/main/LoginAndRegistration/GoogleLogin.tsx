6de8569a6c4080b5ee9e275dac43a89b7f1801cc
evgenia <presidentkenii@gmail.com>
2022-08-12 16:49:49 +0300

    fix google script

--- src/modules/main/LoginAndRegistration/GoogleLogin.tsx
+++ src/modules/main/LoginAndRegistration/GoogleLogin.tsx
@@ -1,28 +1,34 @@
-import React, {useEffect} from "react";
+import React, {useEffect, useState} from "react";
 import {googleLoginAsync} from "../../../store/authSlice";
 import {useAppDispatch} from "../../../store";
 import s from "./auth.module.scss";
+import useScript from "../../util/useScript";
 
 
 export const GoogleLogin = () => {
     let dispatch = useAppDispatch()
+    const [loadGoogle, setLoadGoogle] = useState(false)
     let onSuccessGoogleLogin = (res: any) => {
         if (!res.clientId || !res.credential) return
         dispatch(googleLoginAsync(res.credential))
     }
+    useScript("https://accounts.google.com/gsi/client", () => setLoadGoogle(true))
+
     useEffect(() => {
-        /* global google */
-        // @ts-ignore
-        google.accounts.id.initialize({
-            client_id: process.env.REACT_APP_GOOGLE_CLIENT_ID,
-            callback: onSuccessGoogleLogin
-        })
-        // @ts-ignore
-        google.accounts.id.renderButton(
-            document.getElementById("signInDiv"),
-            {theme: "outline", size: "large"}
-        )
-    }, []);
+        if (loadGoogle) {
+            /* global google */
+            // @ts-ignore
+            google.accounts.id.initialize({
+                client_id: process.env.REACT_APP_GOOGLE_CLIENT_ID,
+                callback: onSuccessGoogleLogin
+            })
+            // @ts-ignore
+            google.accounts.id.renderButton(
+                document.getElementById("signInDiv"),
+                {theme: "outline", size: "large"}
+            )
+        }
+    }, [loadGoogle]);
     return (
         <div className={s.googleBox}>
             <div className={s.googleLogin}>
