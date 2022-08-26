bba9ef3317a27ec95a5a8e9e97677fc61a85a9c9
evgenia <presidentkenii@gmail.com>
2022-08-26 18:53:22 +0300

    google analytics

--- src/modules/App/App.tsx
+++ src/modules/App/App.tsx
@@ -1,5 +1,5 @@
 import React, {useEffect} from "react";
-import {Route, Routes} from "react-router-dom"
+import {Route, Routes, useLocation} from "react-router-dom"
 import EngWord from "../main/EngWord/EngWord";
 import Alerts from "../main/Alert/Alerts";
 import {LoginAndRegistration} from "../main/LoginAndRegistration/LoginAndRegistration";
@@ -18,13 +18,20 @@ import {Consonance} from "../main/consonance/Consonance";
 import useAppTheme from "../util/useAppTheme";
 import {PracticeContainer} from "../main/Practice/PracticeContainer";
 import {StartPageNew} from "../main/StartPage/StartPageNew";
+import ReactGA from "react-ga4";
+
+ReactGA.initialize(process.env.REACT_APP_GOOGLE_ANALYTIC_ID!);
 
 const App: React.FC<any> = () => {
     const {
         isDarkTheme,
         initialized
     } = useAppSelector((state) => state.appReducer);
+    const location = useLocation();
     const dispatch =  useAppDispatch();
+    useEffect(() => {
+        ReactGA.send({ hitType: "pageview", page: location.pathname + location.search});
+    }, [location]);
     useEffect(() => dispatch(initializedAppAsync()), [])
     useAppTheme(isDarkTheme)
     if (!initialized) {
